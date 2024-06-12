6586d58984f34f9b981f88974f321860df19b2c4
j-dewey <jaredewey9@gmail.com>
2024-06-12 12:42:15 -0400

    started scene implementation

--- engine_core/src/lib.rs
+++ engine_core/src/lib.rs
@@ -1,1 +1,2 @@
-
+mod scene;
+mod schedule;
---
+++ engine_core/src/schedule.rs
@@ -0,0 +1,10 @@
+/*
+ * A schedule determines when each update or query gets called
+ * and also ensures that they work properly.
+ *
+ * ex:
+ *    the rendering system can't update until it
+ */
+pub(crate) struct QuerySchedule {}
+
+pub(crate) struct UpdateSchedule {}
