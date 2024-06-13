e55ddb02bf3d5b6fcd1d7f0345717dd21a46152c
j-dewey <jaredewey9@gmail.com>
2024-06-13 00:12:49 -0400

    Moved Systems to engine_core

--- frosty_alloc/src/lib.rs
+++ frosty_alloc/src/lib.rs
@@ -1,23 +1,9 @@
 mod access;
 mod allocator;
 mod chunk;
-mod system;
 
 pub use access::*;
 pub use allocator::Allocator;
 use hashbrown::HashMap;
-pub use system::*;
-
-type Index = usize;
 
 pub unsafe trait FrostyAllocatable {}
-
-// An interface to easily let [System]s
-// interact with an [Allocator]
-pub struct FrostyAllocInterface {
-    alloc: Allocator,
-    named_entries: HashMap<AllocName, Index>,
-    //sys_interops: HashMap,
-}
-
-impl FrostyAllocInterface {}
