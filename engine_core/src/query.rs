a168f0619765b9184065356f9d55498aad29775a
j-dewey <jaredewey9@gmail.com>
2024-07-23 21:43:35 -0400

    Fixed naming

--- engine_core/src/query.rs
+++ engine_core/src/query.rs
@@ -1,1 +1,13 @@
-pub struct Query {}
+use frosty_alloc::ObjectHandleMut;
+
+pub(crate) enum QueryForm {
+    // take all objects at once
+    Continuous,
+    // only take one object at a time
+    Discrete,
+}
+
+pub(crate) struct Query {
+    form: QueryForm,
+    objs: Vec<ObjectHandleMut<u8>>,
+}
