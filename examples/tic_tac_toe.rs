cdaa7d4cbd4f082ba4dfff5c043fdc60337464fb
j-dewey <jcdewey@wm.edu>
2024-10-09 08:24:55 -0400

    Started basic feature tracking

---
+++ examples/tic_tac_toe.rs
@@ -0,0 +1,18 @@
+/*
+ *  This will be used to test that the engine is able to handle simple
+ *  tasks and is capable of running the most basic 2d game. Once this is
+ *  running, more complicated examples will be created
+ *
+ *  TODO:
+ *      - App
+ *      - Render Pipeline
+ *          - 2d Mesh
+ *          - Layering
+ *      - Systems
+ *          - Clicking on squares
+ *          - Checking for finished game
+ */
+
+fn main() {
+    println!("Hello World!")
+}
