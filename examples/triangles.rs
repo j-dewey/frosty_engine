e02dbb97f5f831aef1afbc6682f25c067583dc14
j-dewey <jareddewey9@gmail.com>
2025-07-29 11:27:01 -0400

    Triangles example

---
+++ examples/triangles.rs
@@ -0,0 +1,64 @@
+use basic_3d::camera::Camera3d;
+use basic_3d::render::general_3d_pipeline;
+use cgmath::Rad;
+use engine_core::{App, SceneBuilder};
+use render::{
+    mesh::{IndexArray, Mesh},
+    vertex::MeshVertex,
+    winit::{dpi::PhysicalSize, event_loop::EventLoop, window::WindowBuilder},
+};
+
+fn generate_triangle(v1: [f32; 3], v2: [f32; 3], v3: [f32; 3]) -> Mesh<MeshVertex> {
+    let top = MeshVertex {
+        world_pos: v1,
+        tex_coords: [0.0, 0.0],
+        mat: 0,
+        normal: [0.0, 0.0, 0.0],
+    };
+    let left = MeshVertex {
+        world_pos: v2,
+        tex_coords: [0.0, 0.0],
+        mat: 0,
+        normal: [0.0, 0.0, 0.0],
+    };
+    let right = MeshVertex {
+        world_pos: v3,
+        tex_coords: [0.0, 0.0],
+        mat: 0,
+        normal: [0.0, 0.0, 0.0],
+    };
+    Mesh {
+        verts: vec![top, left, right],
+        indices: IndexArray::new_u32(&[0, 1, 2]),
+    }
+}
+
+fn set_scene(win_size: PhysicalSize<u32>) -> SceneBuilder {
+    SceneBuilder::new()
+        .register_component::<Camera3d>()
+        .register_component::<Mesh<MeshVertex>>()
+        .spawn_component(Camera3d::new_basic(
+            [0.0, 0.0, 0.0],
+            Rad(0.0),
+            Rad(0.0),
+            win_size,
+        ))
+        .spawn_component(generate_triangle(
+            [2.5, 1.0, -1.0],
+            [2.5, -1.0, -2.0],
+            [2.5, -1.0, 0.0],
+        ))
+        .spawn_component(generate_triangle(
+            [2.5, 1.0, 1.0],
+            [2.5, -1.0, 2.0],
+            [2.5, -1.0, 0.0],
+        ))
+        .prep_render_pipeline(&general_3d_pipeline)
+}
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let win_size = window.inner_size();
+    App::new(&window).run(set_scene(win_size), event_loop);
+}
